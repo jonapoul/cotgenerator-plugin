5ef31b7822ccf4fc2cf8c00be9d478c4238e4b0c
jonapoul <jonathanpoulton1@gmail.com>
2021-01-02 19:34:55 +0000

    Removed the redundant preference fragment

--- app/src/main/java/com/jonapoul/cotgenerator/plugin/ui/GeneratorMapComponent.kt
+++ app/src/main/java/com/jonapoul/cotgenerator/plugin/ui/GeneratorMapComponent.kt
@@ -2,15 +2,12 @@ package com.jonapoul.cotgenerator.plugin.ui
 
 import android.content.Context
 import android.content.Intent
-import androidx.core.content.res.ResourcesCompat
 import com.atakmap.android.dropdown.DropDownMapComponent
 import com.atakmap.android.dropdown.DropDownReceiver
 import com.atakmap.android.ipc.AtakBroadcast.DocumentedIntentFilter
 import com.atakmap.android.maps.MapView
 import com.atakmap.android.toolbar.ToolManagerBroadcastReceiver
-import com.atakmap.app.preferences.ToolsPreferenceFragment
 import com.jonapoul.cotgenerator.plugin.R
-import com.jonapoul.cotgenerator.plugin.prefs.GeneratorPreferenceFragment
 import com.jonapoul.cotgenerator.plugin.tool.CentrePointPickerTool
 import com.jonapoul.cotgenerator.plugin.utils.Intents
 import timber.log.Timber
@@ -43,24 +40,6 @@ class GeneratorMapComponent : DropDownMapComponent() {
 
         /* Set up a widget to show activity status, and give a quick link to open the window */
         statusWidget = GeneratorStatusWidget(mapView, pluginContext, dropdownReceiver)
-
-        /* Register the preference fragment */
-        ToolsPreferenceFragment.register(
-            ToolsPreferenceFragment.ToolPreference(
-                pluginContext.getString(R.string.preferences_title),
-                pluginContext.getString(R.string.preferences_summary),
-                pluginContext.getString(R.string.preferences_key),
-                ResourcesCompat.getDrawable(
-                    pluginContext.resources,
-                    R.drawable.plugin_icon,
-                    null
-                ),
-                GeneratorPreferenceFragment(
-                    pluginContext,
-                    appContext = mapView.context
-                )
-            )
-        )
     }
 
     override fun onDestroyImpl(context: Context?, view: MapView?) {
